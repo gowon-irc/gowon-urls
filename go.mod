46bbd700ea18653a108bce46e62a845e119026fd
shanedabes <shane@isda.best>
2021-10-17 01:39:08 +0100

    handle autoreconnect

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.3.5
-	github.com/gowon-irc/go-gowon v0.0.0-20211015001505-9685ff9cabf5
+	github.com/gowon-irc/go-gowon v0.0.0-20211016234342-d666c7430ea9
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/stretchr/testify v1.7.0
 )
@@ -14,7 +14,7 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
-	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -22,7 +22,9 @@ type Options struct {
 }
 
 const (
-	mqttConnectRetryInternal = 5 * time.Second
+	moduleName               = "jod"
+	mqttConnectRetryInternal = 5
+	mqttDisconnectTimeout    = 1000
 )
 
 func genUrlHandler(filters []*regexp.Regexp) func(m gowon.Message) (string, error) {
@@ -36,23 +38,41 @@ func genUrlHandler(filters []*regexp.Regexp) func(m gowon.Message) (string, erro
 	}
 }
 
-func main() {
-	opts := Options{}
+func defaultPublishHandler(c mqtt.Client, msg mqtt.Message) {
+	log.Printf("unexpected message:  %s\n", msg)
+}
 
-	_, err := flags.Parse(&opts)
-	if err != nil {
+func onConnectionLostHandler(c mqtt.Client, err error) {
+	log.Println("connection to broker lost")
+}
+
+func onRecconnectingHandler(c mqtt.Client, opts *mqtt.ClientOptions) {
+	log.Println("attempting to reconnect to broker")
+}
+
+func onConnectHandler(c mqtt.Client) {
+	log.Println("connected to broker")
+}
+
+func main() {
+	log.Printf("%s starting\n", moduleName)
+
+	opts := Options{}
+	if _, err := flags.Parse(&opts); err != nil {
 		log.Fatal(err)
 	}
 
-	mqttOpts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
-	mqttOpts.SetClientID("gowon_urls")
+	mqttOpts := mqtt.NewClientOptions()
+	mqttOpts.AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
+	mqttOpts.SetClientID(fmt.Sprintf("gowon_%s", moduleName))
 	mqttOpts.SetConnectRetry(true)
-	mqttOpts.SetConnectRetryInterval(mqttConnectRetryInternal)
+	mqttOpts.SetConnectRetryInterval(mqttConnectRetryInternal * time.Second)
+	mqttOpts.SetAutoReconnect(true)
 
-	c := mqtt.NewClient(mqttOpts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
+	mqttOpts.DefaultPublishHandler = defaultPublishHandler
+	mqttOpts.OnConnectionLost = onConnectionLostHandler
+	mqttOpts.OnReconnecting = onRecconnectingHandler
+	mqttOpts.OnConnect = onConnectHandler
 
 	filters := []*regexp.Regexp{}
 	for _, f := range opts.Filters {
@@ -62,10 +82,23 @@ func main() {
 
 	mr := gowon.NewMessageRouter()
 	mr.AddRegex(urlRegex, genUrlHandler(filters))
-	mr.Subscribe(c, "gowon-urls")
+	mr.Subscribe(mqttOpts, moduleName)
+
+	log.Print("connecting to broker")
+
+	c := mqtt.NewClient(mqttOpts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	log.Print("connected to broker")
 
 	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	<-sigs
+
+	log.Println("signal caught, exiting")
+	c.Disconnect(mqttDisconnectTimeout)
+	log.Println("shutdown complete")
 }
